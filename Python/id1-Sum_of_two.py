3e850934b65e5dc64749067e37b0a305f707f8db
Michael Arnold <valestrum@hotmail.com>
2014-11-30 06:00:49 -0500

    Create id1-Sum_of_two.py

---
+++ Python/id1-Sum_of_two.py
@@ -0,0 +1,4 @@
+def add(a, b):                                                                                                                                                  
+        c = (a + b)
+        return c
+print add(9812989, 3395698)
