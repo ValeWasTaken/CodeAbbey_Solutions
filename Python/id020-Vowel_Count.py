bfddae52acd9c1cbf2f40a84c511eb9ccd8f5e84
Michael Arnold <valestrum@hotmail.com>
2014-12-13 22:32:50 -0500

    Create id020-Vowel_Count.py

---
+++ Python/id020-Vowel_Count.py
@@ -0,0 +1,9 @@
+def vowelCount(words):
+        vowels = list("aeiouy")
+        answer = []
+        for x in range(words):
+                phraseCount = raw_input()
+                phraseCount = sum(phraseCount.count(c) for c in vowels)
+                answer.append(str(phraseCount))
+        print(' '.join(answer))
+vowelCount(input())
