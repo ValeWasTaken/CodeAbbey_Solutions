1e5e34ec418594083ad0262ebf8c3ccdd22b0567
Michael Arnold <valestrum@hotmail.com>
2014-12-30 19:06:42 -0500

    Fixed program to count uppercase as well.

--- Python/id020-Vowel_Count.py
+++ Python/id020-Vowel_Count.py
@@ -2,7 +2,7 @@ def vowelCount(words):
         vowels = list("aeiouy")
         answer = []
         for x in range(words):
-                phraseCount = raw_input()
+                phraseCount = raw_input().lower()
                 phraseCount = sum(phraseCount.count(c) for c in vowels)
                 answer.append(str(phraseCount))
         print(' '.join(answer))
