fc51283bf1d80ea86a2987c1f331bd3a3d6b1b3a
Michael Arnold <valestrum@hotmail.com>
2015-01-02 21:11:59 -0500

    Optimized solution
    Removed variables and changed while loop to a for loop.

--- Python/id003-Sums_in_loop.py
+++ Python/id003-Sums_in_loop.py
@@ -1,13 +1,10 @@
 def find_Sums(pairAmount):
-        pairCount = 0
         answer = []
-        while pairCount < pairAmount:
-                digits = raw_input()
-                numbers = digits.split(' ')
+        for pair in range(pairAmount):
+                numbers = raw_input().split(' ')
                 sum = 0
                 for x in range(0, 2):
                         sum += int(numbers[x])
                 answer.append(str(sum))
-                pairCount += 1
         print(' '.join(answer))
 find_Sums(input())
