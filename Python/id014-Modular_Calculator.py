184717a9295d0a6022479ee1fdafd25f569a4468
Michael Arnold <valestrum@hotmail.com>
2015-01-06 20:59:09 -0500

    Create id014-Modular_Calculator.py

---
+++ Python/id014-Modular_Calculator.py
@@ -0,0 +1,12 @@
+def modCalc(num):
+        calc = raw_input()
+        while calc[:1] != "%":
+                if calc[:1] == '+':
+                        num += int(calc[2:].strip())
+                        calc = raw_input()
+                elif calc[:1] == '*':
+                        num *= int(calc[2:].strip())
+                        calc = raw_input()
+        num %= int(calc[2:].strip()) 
+        print(num)
+modCalc(input())
