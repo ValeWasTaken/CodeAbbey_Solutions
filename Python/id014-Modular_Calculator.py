343d69ef63b764f1ef73d76fcb5ba87e0a288728
Michael Arnold <valestrum@hotmail.com>
2015-01-08 23:08:36 -0500

    Optimized program slightly

--- Python/id014-Modular_Calculator.py
+++ Python/id014-Modular_Calculator.py
@@ -3,10 +3,9 @@ def modCalc(num):
         while calc[:1] != "%":
                 if calc[:1] == '+':
                         num += int(calc[2:].strip())
-                        calc = raw_input()
                 elif calc[:1] == '*':
                         num *= int(calc[2:].strip())
-                        calc = raw_input()
+                calc = raw_input()
         num %= int(calc[2:].strip()) 
         print(num)
 modCalc(input())
