73ffedbeb6960d628208c1e442d0d97f739ba0dc
Michael Arnold <valestrum@hotmail.com>
2015-01-20 21:06:08 -0500

    Further improved program
    Removed unimportant lines of code, condensing the majority of the program into:
    
    "answer.append(str(min(int(pair[0]),int(pair[1]))))"

--- Python/id004-Minimum_of_Two.py
+++ Python/id004-Minimum_of_Two.py
@@ -1,12 +1,7 @@
 def findMin(amount):
-        pairCount = 0
         answer = []
         for pairCount in range(amount):
                 pair = raw_input().split(' ')
-                num1,num2 = int(pair[0]),int(pair[1])
-                if num1 < num2:
-                        answer.append(str(num1))
-                else:
-                        answer.append(str(num2))
+                answer.append(str(min(int(pair[0]),int(pair[1]))))
         print(' '.join(answer))
 findMin(input())
