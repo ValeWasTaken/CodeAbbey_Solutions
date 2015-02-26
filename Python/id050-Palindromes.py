9a42b1dd7bba4cd433a34734ab8c5bbba95ab25d
Michael Arnold <valestrum@hotmail.com>
2015-02-25 19:57:25 -0500

    Slightly optimized program

--- Python/id050-Palindromes.py
+++ Python/id050-Palindromes.py
@@ -1,22 +1,21 @@
 def cleanWord(data):
-        word = []
-        for char in data:
-                if str.isalpha(char) == True:
-                        word.append(char)
-                elif str.isalpha(char) == False:
-                        char = ''
-                        word.append(char)
-        return word
+    word = []
+    for char in data:
+        if str.isalpha(char) == True:
+            word.append(char)
+        else:
+            word.append('')
+    return word
 
 def isPalindrome(wordCount):
-        answer = []
-        for x in range(wordCount):
-                word = cleanWord(raw_input().lower())
-                word = ''.join(word)
-                if word == word[::-1]:
-                        answer.append('Y')
-                elif word != word[::-1]:
-                        answer.append('N')
-        print(' '.join(answer))
+    answer = []
+    for x in range(wordCount):
+        word = cleanWord(raw_input().lower())
+        word = ''.join(word)
+        if word == word[::-1]:
+            answer.append('Y')
+        else:
+            answer.append('N')
+    print(' '.join(answer))
 
 isPalindrome(input())
