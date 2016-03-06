3b53594573e5cbab5b7cdbc50fbe035528138550
Michael Arnold <valestrum@hotmail.com>
2016-03-05 23:05:56 -0500

    Updated for Python 3.4

--- Python/id002-Sum_in_loop.py
+++ Python/id002-Sum_in_loop.py
@@ -1,7 +1,3 @@
-def find_Sum(amount, digits):                                                                                                                                   
-        numbers = digits.split(' ')
-        sum = 0
-        for x in range(amount):
-                sum += int(numbers[x])
-        print(sum)
-find_Sum(input(), raw_input())
+# Python 3.4
+code_abbey_wasted_variable = input()
+print(sum(int(x) for x in input().split()))
