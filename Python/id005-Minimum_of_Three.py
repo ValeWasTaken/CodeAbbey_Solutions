c7a811acb1746eb04c07d54d77d46da87148cff8
Michael Arnold <valestrum@hotmail.com>
2016-03-06 23:52:34 -0500

    Optimized solution and converted to Python 3.4

--- Python/id005-Minimum_of_Three.py
+++ Python/id005-Minimum_of_Three.py
@@ -1,8 +1,11 @@
-def findMin(amount):
-        answer = []
-        for x in range(amount):
-                [a, b, c] = raw_input().split()
-                minNum = min(int(a), int(b), int(c))
-                answer.append(str(minNum))
-        print(' '.join(answer))
-findMin(input())
+# Python 3.4
+def min_of_three():
+    runs = int(input())
+    answer = []
+    
+    for run in range(runs):
+        answer.append(str(min([int(x) for x in input().split()])))
+        
+    print(' '.join(answer))
+
+min_of_three()
